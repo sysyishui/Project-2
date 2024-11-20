9c8907bb829efd342d819a1a160952279714cae1
sysyishui <145036157+sysyishui@users.noreply.github.com>
2024-11-19 21:37:26 -0500

    Add files via upload

---
+++ Main Menu Scripts/ExitButtonScript.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ExitButtonScript : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
